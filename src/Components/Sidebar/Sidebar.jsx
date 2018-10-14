bda5ad068989c8de03f9eb327fc410449b169555
Aman Agarwal <cyclopaman@gmail.com>
2018-10-14 15:41:01 +0530

    Sidebar Initiated

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+import "./Sidebar.css";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faGhost } from "@fortawesome/free-solid-svg-icons";
+
+// library.add(faGhost);
+class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {
+        imgURL: "https://picsum.photos/120/120"
+    };
+    render() {
+        return (
+            <React.Fragment>
+                <img src={this.state.imgURL} alt="" />
+                <div className="menuItems">
+                    {/* <FontAwesomeIcon icon="ghost" /> */}
+                    Home
+                </div>
+                <div className="menuItems">SkillSet</div>
+                <div className="menuItems">Education</div>
+                <div className="menuItems">Projects</div>
+                <div className="menuItems">Experience</div>
+                <div className="menuItems">Social Apperaence</div>
+                <div className="menuItems">Contact ME</div>
+                <div className="menuItems">Dev Stack</div>
+            </React.Fragment>
+        );
+    }
+}
+export default Sidebar;
